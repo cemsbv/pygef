82ef15a93831eb783fa08e2769064209df1fcb6f
Martina <martinapippi16@gmail.com>
2019-01-14 17:05:12 +0100

    new-robertson

--- pygef/robertson/run_robertson.py
+++ pygef/robertson/run_robertson.py
@@ -1,5 +1,6 @@
 from pygef.robertson.robertson import Robertson
-from pygef.robertson.new_robertson import Newrobertson
+from pygef.robertson.robertson import NewRobertson
+
 import os
 
 
@@ -11,18 +12,15 @@ def scan_folder(parent):
                 path = parent + folder + '/' + file_name
                 print(file_name)
                 ciao_rob = Robertson(path)
-                ciao_new_rob = Newrobertson(path)
+                ciao_new_rob = NewRobertson(path)
                 df = ciao_rob.df_complete
-
                 print(df)
 
 
-
-
 #scan_folder("/home/martina/Documents/gef_files/2013/")
-rob = Robertson("/home/martina/Documents/gef_files/2018/18337/18337_AA17425_42.GEF")
-rob2 = Newrobertson("/home/martina/Documents/gef_files/2018/18337/18337_AA17425_42.GEF")
-df = rob.df_complete
+#rob = Robertson("/home/martina/Documents/gef_files/2018/18337/18337_AA17425_42.GEF")
+rob2 = NewRobertson("/home/martina/Documents/gef_files/2018/18337/18337_AA17425_42.GEF")
+#df = rob.df_complete
 df2 = rob2.df_complete
-print(df)
+#print(df)
 print(df2)
