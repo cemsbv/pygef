486ab6935b9af7577f7770db46c80787616110b3
martinapippi <46439957+martinapippi@users.noreply.github.com>
2019-02-21 13:37:57 +0100

    Gef only (#5)
    * big unit test borehole file
    
    * read me and ci/cd
    
    * plot_cpt and image

--- pygef/plot_cpt.py
+++ pygef/plot_cpt.py
@@ -8,19 +8,18 @@ class PlotCPT:
     def plot_cpt(self, show=True):
         depth_max = self.df['depth'].max()
         depth_min = self.df['depth'].min()
-        fig = plt.figure(figsize=(15, 30))
+        fig = plt.figure(figsize=(8, 30))
 
-        qc = fig.add_subplot(1, 4, 1)  # Plot qc
-        plt.plot(self.df['qc'], self.df['depth'], 'b')
-        qc.set_xlabel('qc (MPa)')
-        qc.set_ylabel('Z (m)')
-        plt.ylim(depth_max, depth_min)
+        n = 0
+        for c, unit in zip(['qc', 'Fr'], ['[MPa]', '[%]']):
+            n += 1
+            qc = fig.add_subplot(1, 2, n)
+            plt.plot(self.df[c], self.df['depth'], 'b')
+            qc.set_xlabel(f'{c} {unit}')
+            qc.set_ylabel('Z [m]')
+            plt.grid()
+            plt.ylim(depth_max, depth_min)
 
-        Fr = fig.add_subplot(1, 4, 2)
-        plt.plot(self.df['Fr'], self.df['depth'], 'b')
-        Fr.set_xlabel('Fr (%)')
-        Fr.set_ylabel('Z (m)')
-        plt.ylim(depth_max, depth_min)
         if show:
             return plt.show()
         return fig
