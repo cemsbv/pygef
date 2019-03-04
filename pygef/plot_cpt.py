588111434260fe9eb8f3fcecd28ebd26a650f656
Martina <martinapippi16@gmail.com>
2019-03-04 11:42:44 +0100

    added classiy_soil

--- pygef/plot_cpt.py
+++ pygef/plot_cpt.py
@@ -1,9 +1,23 @@
 import matplotlib.pyplot as plt
+from pygef.gef import ParseGEF
 
 
 class PlotCPT:
-    def __init__(self, df):
-        self.df = df
+    def __init__(self, df, classification, water_level_NAP, p_a, new=True):
+        area_quotient_cone_tip = ParseGEF.area_quotient_cone_tip
+        pre_excavated_depth = ParseGEF.pre_excavated_depth
+
+        self.df = df.assign(self.classify_soil, classification, water_level_NAP,
+                            area_quotient_cone_tip, pre_excavated_depth, p_a, new=new)
+    @staticmethod
+    def classify_soil(classification, water_level_NAP, area_quotient_cone_tip, pre_excavated_depth, p_a,
+                      new=True):
+        if classification == 'robertson':
+            return ParseGEF.classify_robertson(water_level_NAP, new, area_quotient_cone_tip=area_quotient_cone_tip,
+                                               pre_excavated_depth=pre_excavated_depth, p_a=p_a)
+        elif classification == 'been_jeffrey':
+            return ParseGEF.classify_been_jeffrey(water_level_NAP, area_quotient_cone_tip=area_quotient_cone_tip,
+                                               pre_excavated_depth=pre_excavated_depth)
 
     def plot_cpt(self, show=True, figsize=(8, 30)):
         depth_max = self.df['depth'].max()
@@ -21,7 +35,5 @@ class PlotCPT:
         if show:
             return plt.show()
         return fig
-
-
 
 
