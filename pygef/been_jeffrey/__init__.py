952137477e8c6e2682f6f35c50ab20127b79a010
Martina <martinapippi16@gmail.com>
2019-04-10 14:03:09 +0200

    water_level_NAP been_jeffrey

--- pygef/been_jeffrey/__init__.py
+++ pygef/been_jeffrey/__init__.py
@@ -3,8 +3,8 @@ from pygef.been_jeffrey.util import iterate_been_jeffrey
 
 def classify(df, water_level_and_zid_NAP=None, water_level_wrt_depth=None, area_quotient_cone_tip=None,
              pre_excavated_depth=None,):
-    if water_level_and_zid_NAP is not None:
-        water_level = water_level_and_zid_NAP['zid'] - water_level_and_zid_NAP['water_level']
+    if water_level_and_zid_NAP['water_level_NAP'] is not None:
+        water_level = water_level_and_zid_NAP['zid'] - water_level_and_zid_NAP['water_level_NAP']
     elif water_level_wrt_depth is not None:
         water_level = water_level_wrt_depth
     else:
