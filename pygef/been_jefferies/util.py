72a082d09b5010c453c23cde412ee1958038bbf0
Ritchie Vink <ritchie46@gmail.com>
2021-08-30 12:46:37 +0200

    refactor ic_to_gamma

--- pygef/been_jefferies/util.py
+++ pygef/been_jefferies/util.py
@@ -1,5 +1,6 @@
 import numpy as np
 import polars as pl
+from polars import col
 
 import pygef.utils as utils
 from pygef import geo
@@ -24,39 +25,26 @@ def excess_pore_pressure_ratio(df):
     return df
 
 
-def ic_to_gamma(df, water_level):
+def ic_to_gamma(water_level):
     """
-    Assign the right gamma (unit soil weight kN/m^3) to the corresponding Ic.
-
-    :param df: (DataFrame) Original DataFrame.
-    :param water_level: (int) Water level with respect to ground level.
-    :return: Updated DataFrame.
+    Return the expression needed to compute "gamma_predict"
     """
-    mask_below_water = (1.0 - df["depth"]) < water_level
-    # TODO: how to fill it properly with the same initial values?
-    df["gamma_predict"] = np.tile(1.0, len(df.rows()))
-
-    ic_mask = df["type_index"] > 3.22
-    # gamma_(sat) and ic > 3.6
-    df[ic_mask, "gamma_predict"] = 11.0
-
-    ic_mask = df["type_index"] <= 3.22
-    # gamma_(sat) and ic < 3.6
-    df[ic_mask, "gamma_predict"] = 16.0
-
-    ic_mask = df["type_index"] <= 2.76
-    # gamma_(sat) and ic < x
-    df[ic_mask, "gamma_predict"] = 18.0
-
-    ic_mask = df["type_index"] <= 2.40
-    # gamma_sat and ic < x
-    df[ic_mask & mask_below_water, "gamma_predict"] = 19.0
-
-    ic_mask = df["type_index"] <= 1.80
-    # gamma_sat and ic < x
-    df[ic_mask & mask_below_water, "gamma_predict"] = 20.0
-
-    return df
+    below_water = (1.0 - col("depth")) < water_level
+    ti = col("type_index")
+    return (
+        pl.when(ti > 3.22)
+        .then(11.0)
+        .when((ti <= 3.22) & (ti > 2.76))
+        .then(16.0)
+        .when((ti <= 2.76) & ~(below_water))
+        .then(18.0)
+        .when(ti <= 2.40 & below_water)
+        .then(19.0)
+        .when(ti <= 1.80 & below_water)
+        .then(20.0)
+        .otherwise(1.0)
+        .alias("gamma_predict")
+    )
 
 
 def ic_to_soil_type(df):
@@ -173,6 +161,6 @@ def been_jeffrey(
         .pipe(geo.normalized_friction_ratio)
         .pipe(utils.none_to_zero)
         .pipe(type_index)
-        .pipe(ic_to_gamma, water_level)
+        .with_column(ic_to_gamma(water_level))
     )
     return df
