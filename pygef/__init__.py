7cdc59e85938ecd73bafb4d36d60976ee49e3d7d
Ritchie Vink <ritchie46@gmail.com>
2022-08-22 11:42:53 +0200

    set strict mypy rules for broxml and fix issues

--- pygef/__init__.py
+++ pygef/__init__.py
@@ -3,3 +3,6 @@ from pygef.bore import Bore
 from pygef.cpt import Cpt
 from pygef.plot_utils import plot_merged_cpt_bore
 from pygef.utils import depth_to_nap, join_gef, nap_to_depth
+
+
+__all__ = ["cpt", "bore", "broxml"]
--- pygef/broxml/cpt.py
+++ pygef/broxml/cpt.py
@@ -1,2 +1,9 @@
-# class BroCPT:
-#     def __init__(self, f: io.):
+from __future__ import annotations
+
+import io
+from pathlib import Path
+
+
+class BroCPT:
+    def __init__(self, file: io.BytesIO | Path | str):
+        self.file = file
