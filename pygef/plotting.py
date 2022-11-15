f040df8ac4f2c7f0a69c8a469d6e480d46534ea5
Ritchie Vink <ritchie46@gmail.com>
2022-11-15 10:10:16 +0100

    add simple bore plotting

--- pygef/plotting.py
+++ pygef/plotting.py
@@ -1,7 +1,22 @@
 from __future__ import annotations
+
+from typing import Tuple
+
 import matplotlib.pyplot as plt
-from pygef import CPTData
+import matplotlib.patches as mpatches
+from pygef import CPTData, BoreData
 import polars as pl
+import numpy as np
+
+
+def init_fig(
+    fig: plt.Figure | None, dpi: int, figsize: Tuple[int, int] | None
+) -> Tuple[plt.Figure, bool]:
+    figure_given = True
+    if fig is None:
+        figure_given = False
+        fig = plt.figure(figsize=figsize, dpi=dpi)
+    return fig, figure_given
 
 
 def plot_cpt(
@@ -11,10 +26,7 @@ def plot_cpt(
         pl.when(pl.all().abs() > 1e4).then(None).otherwise(pl.all()).keep_name()
     )
 
-    figure_given = True
-    if fig is None:
-        figure_given = False
-        fig = plt.figure(figsize=(8, 12), dpi=dpi)
+    fig, figure_given = init_fig(fig, dpi, figsize=(8, 12))
 
     axs = fig.subplots(1, 3, gridspec_kw={"width_ratios": [2, 1, 1]})
     (ax1, ax2, ax3) = axs
@@ -46,6 +58,41 @@ def plot_cpt(
         depth,
     )
     ax2.legend()
+
+    if figure_given:
+        return fig
+    else:
+        plt.show()
+        return None
+
+
+def plot_bore(
+    data: BoreData, fig: plt.Figure | None, dpi: int = 100
+) -> plt.Figure | None:
+    fig, figure_given = init_fig(fig, dpi, figsize=(4, 12))
+    ax = fig.subplots(1, 1)
+
+    df = data.data
+    soil_dist = np.stack(df["soil_dist"].to_numpy())  # type: ignore[call-overload]
+    cum_soil_dist = soil_dist.cumsum(axis=1)
+
+    # peat, clay, silt, sand, gravel, rocks
+    c = ["#a76b29", "#578E57", "#0078C1", "#DBAD4B", "#708090", "#59626b"]
+    legend = ["peat", "clay", "silt", "sand", "gravel", "rocks"]
+    ax.invert_yaxis()
+    ax.set_ylabel("depth [m]")
+    ax.set_xlabel("cumulative soil fraction")
+
+    for i in range(5):
+        ax.fill_betweenx(
+            np.repeat(df["upper_boundary"], 2),
+            np.zeros(df.shape[0] * 2),
+            np.roll(np.repeat(cum_soil_dist[:, -(i + 1)], 2), 1),
+            color=c[i],
+        )
+
+    patches = [mpatches.Patch(color=color, label=key) for key, color in zip(legend, c)]
+    plt.legend(handles=patches, bbox_to_anchor=(1, 1), loc="upper left")
 
     if figure_given:
         return fig
