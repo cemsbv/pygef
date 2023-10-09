c93ea8ac147d5ca4705e6694d52497efc25fec5c
Robin Wimmers <r.wimmers@cemsbv.io>
2023-10-09 09:53:40 +0200

    fix: resolve #354 update xpath

--- src/pygef/broxml/parse_cpt.py
+++ src/pygef/broxml/parse_cpt.py
@@ -18,176 +18,176 @@ from pygef.cpt import CPTData
 CPT_ATTRIBS = {
     "bro_id": {"xpath": "brocom:broId"},
     "research_report_date": {
-        "xpath": "./researchReportDate/brocom:date",
+        "xpath": "ns11:researchReportDate/brocom:date",
         "resolver": resolvers.parse_date,
         "el-attr": "text",
     },
-    "cpt_standard": {"xpath": "cptStandard"},
+    "cpt_standard": {"xpath": "ns11:cptStandard"},
     "delivered_location": {
-        "xpath": "./deliveredLocation/cptcommon:location",
+        "xpath": "ns11:deliveredLocation/cptcommon:location",
         "resolver": resolvers.parse_gml_location,
     },
     "standardized_location": {
-        "xpath": "./standardizedLocation/brocom:location",
+        "xpath": "ns11:standardizedLocation/brocom:location",
         "resolver": resolvers.parse_gml_location,
     },
     "dissipationtest_performed": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:dissipationTestPerformed",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:dissipationTestPerformed",
         "resolver": resolvers.parse_bool,
         "el-attr": "text",
     },
     "quality_class": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:qualityClass",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:qualityClass",
         "resolver": resolvers.parse_quality_class,
         "el-attr": "text",
     },
     "groundwater_level": {
-        "xpath": "./additionalInvestigation/cptcommon:groundwaterLevel",
+        "xpath": "ns11:additionalInvestigation/cptcommon:groundwaterLevel",
         "resolver": resolvers.parse_float,
         "el-attr": "text",
     },
     "predrilled_depth": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:trajectory/cptcommon:predrilledDepth",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:trajectory/cptcommon:predrilledDepth",
         "resolver": resolvers.parse_float,
         "el-attr": "text",
     },
     "final_depth": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:trajectory/cptcommon:finalDepth",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:trajectory/cptcommon:finalDepth",
         "resolver": resolvers.parse_float,
         "el-attr": "text",
     },
     "data": {
-        "xpath": "./conePenetrometerSurvey",
+        "xpath": "ns11:conePenetrometerSurvey",
         "resolver": resolvers.process_cpt_result,
     },
     "cpt_description": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:description",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:description",
     },
     "cpt_type": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:conePenetrometerType",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:conePenetrometerType",
     },
     "cone_surface_area": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:coneSurfaceArea",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:coneSurfaceArea",
         "resolver": resolvers.parse_int,
         "el-attr": "text",
     },
     "cone_diameter": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:coneDiameter",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:coneDiameter",
         "resolver": resolvers.parse_int,
         "el-attr": "text",
     },
     "cone_surface_quotient": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:coneSurfaceQuotient",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:coneSurfaceQuotient",
         "resolver": resolvers.parse_float,
         "el-attr": "text",
     },
     "cone_to_friction_sleeve_distance": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:coneToFrictionSleeveDistance",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:coneToFrictionSleeveDistance",
         "resolver": resolvers.parse_int,
         "el-attr": "text",
     },
     "cone_to_friction_sleeve_surface_area": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:frictionSleeveSurfaceArea",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:frictionSleeveSurfaceArea",
         "resolver": resolvers.parse_int,
         "el-attr": "text",
     },
     "cone_to_friction_sleeve_surface_quotient": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:frictionSleeveSurfaceQuotient",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:frictionSleeveSurfaceQuotient",
         "resolver": resolvers.parse_float,
         "el-attr": "text",
     },
     "zlm_cone_resistance_before": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:coneResistanceBefore",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:coneResistanceBefore",
         "resolver": resolvers.parse_float,
         "el-attr": "text",
     },
     "zlm_cone_resistance_after": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:coneResistanceAfter",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:coneResistanceAfter",
         "resolver": resolvers.parse_float,
         "el-attr": "text",
     },
     "zlm_inclination_ew_before": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:inclinationEWBefore",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:inclinationEWBefore",
         "resolver": resolvers.parse_int,
         "el-attr": "text",
     },
     "zlm_inclination_ew_after": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:inclinationEWAfter",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:inclinationEWAfter",
         "resolver": resolvers.parse_int,
         "el-attr": "text",
     },
     "zlm_inclination_ns_before": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:inclinationNSBefore",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:inclinationNSBefore",
         "resolver": resolvers.parse_int,
         "el-attr": "text",
     },
     "zlm_inclination_ns_after": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:inclinationNSAfter",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:inclinationNSAfter",
         "resolver": resolvers.parse_int,
         "el-attr": "text",
     },
     "zlm_inclination_resultant_before": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:inclinationResultantBefore",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:inclinationResultantBefore",
         "resolver": resolvers.parse_int,
         "el-attr": "text",
     },
     "zlm_inclination_resultant_after": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:inclinationResultantAfter",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:inclinationResultantAfter",
         "resolver": resolvers.parse_int,
         "el-attr": "text",
     },
     "zlm_local_friction_before": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:localFrictionBefore",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:localFrictionBefore",
         "resolver": resolvers.parse_float,
         "el-attr": "text",
     },
     "zlm_local_friction_after": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:localFrictionAfter",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:localFrictionAfter",
         "resolver": resolvers.parse_float,
         "el-attr": "text",
     },
     "zlm_pore_pressure_u1_before": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:porePressureU1Before",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:porePressureU1Before",
         "resolver": resolvers.parse_float,
         "el-attr": "text",
     },
     "zlm_pore_pressure_u2_before": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:porePressureU2Before",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:porePressureU2Before",
         "resolver": resolvers.parse_float,
         "el-attr": "text",
     },
     "zlm_pore_pressure_u3_before": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:porePressureU3Before",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:porePressureU3Before",
         "resolver": resolvers.parse_float,
         "el-attr": "text",
     },
     "zlm_pore_pressure_u1_after": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:porePressureU1After",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:porePressureU1After",
         "resolver": resolvers.parse_float,
         "el-attr": "text",
     },
     "zlm_pore_pressure_u2_after": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:porePressureU2After",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:porePressureU2After",
         "resolver": resolvers.parse_float,
         "el-attr": "text",
     },
     "zlm_pore_pressure_u3_after": {
-        "xpath": "./conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:porePressureU3After",
+        "xpath": "ns11:conePenetrometerSurvey/cptcommon:conePenetrometer/cptcommon:zeroLoadMeasurement/cptcommon:porePressureU3After",
         "resolver": resolvers.parse_float,
         "el-attr": "text",
     },
     "delivered_vertical_position_offset": {
-        "xpath": "./deliveredVerticalPosition/cptcommon:offset",
+        "xpath": "ns11:deliveredVerticalPosition/cptcommon:offset",
         "resolver": resolvers.parse_float,
         "el-attr": "text",
     },
     "delivered_vertical_position_datum": {
-        "xpath": "./deliveredVerticalPosition/cptcommon:verticalDatum",
+        "xpath": "ns11:deliveredVerticalPosition/cptcommon:verticalDatum",
         "resolver": resolvers.parse_local_vertical_reference_point_class,
         "el-attr": "text",
     },
     "delivered_vertical_position_reference_point": {
-        "xpath": "./deliveredVerticalPosition/cptcommon:localVerticalReferencePoint",
+        "xpath": "ns11:deliveredVerticalPosition/cptcommon:localVerticalReferencePoint",
         "resolver": resolvers.lower_text,
         "el-attr": "text",
     },
@@ -199,4 +199,4 @@ def read_cpt(file: io.BytesIO | Path | str) -> list[CPTData]:
         root = etree.fromstring(file).getroot()
     else:
         root = etree.parse(file).getroot()
-    return read_xml(root, CPTData, CPT_ATTRIBS, "dispatchDocument")
+    return read_xml(root, CPTData, CPT_ATTRIBS, "ns11:dispatchDocument")
