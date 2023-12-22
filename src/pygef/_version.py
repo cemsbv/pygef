1e343da465404dae402899f74e986e4c14085120
Robin Wimmers <r.wimmers@cemsbv.io>
2023-12-22 13:53:05 +0100

    fix: resolve pkg_resources deprecation warning

--- src/pygef/_version.py
+++ src/pygef/_version.py
@@ -1,7 +1,1 @@
-import pkg_resources
-
-try:
-    __version__ = pkg_resources.get_distribution("pygef").version
-# during CI
-except pkg_resources.DistributionNotFound:
-    __version__ = "unknown"
+__version__ = "0.10.0"
